cd8f85c3aae800a13e958f0a269d68b02e7a08d4
Thalles Rangel <ranglethr@gmail.com>
2020-07-29 22:42:41 -0300

    alterações

--- views/template.php
+++ views/template.php
@@ -86,15 +86,22 @@
             <div id="content">
 
                 <nav class="navbar navbar-expand-lg shadow-sm bg-white mb-4 static-top">
-                    <a class="nav-link " href="<?=BASE_URL?>">Início</a>
-
+                
                     <ul class="navbar-nav ml-auto">
-                       
 
-                        <div class="btn-group">
+                        <a class="nav-link" href="<?=BASE_URL?>">
+                            <i class="icon-gray" width="22" data-feather="home"></i>
+                        </a>
+
+                        <a class="nav-link mr-3" href="#">
+                            <i class="icon-gray" width="22" data-feather="bell"></i>
+                        </a>
+
+                        <div class="nav-link btn-group">
                         <a class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <span><?=$_SESSION['nome_usuario']?></span>
                         </a>
+                        
                         <div class="dropdown-menu dropdown-menu-right mt-4">
                             <a href="<?=BASE_URL?>usuario/perfil" class="dropdown-item font-14" type="button"><i class="icon-gray-submenu" width="22" data-feather="user"></i> Perfil</a>
                             <a href="<?=BASE_URL?>configuracao" class="dropdown-item font-14" type="button"><i class="icon-gray-submenu" width="22" data-feather="settings"></i> Configurações</a>
