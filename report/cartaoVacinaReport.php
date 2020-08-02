a236b4cf8841521cb3d00f0b0c10f64f42c0338c
Thalles Rangel <ranglethr@gmail.com>
2020-08-02 12:19:26 -0300

    alterações

--- report/cartaoVacinaReport.php
+++ report/cartaoVacinaReport.php
@@ -1,24 +1,4 @@
-<?php
-
-require_once 'vendor/autoload.php';
-ob_start();
-?>
-
-<style>
-
-.oi{ 
-    background: red;
-    width: 200px;
-}
-</style>
-
-<div class="oi">oi</div>
-
-
-
-<?php
-$html = ob_get_contents();
-$mpdf = new \Mpdf\Mpdf(['mode' => 'utf-8','format' => 'A4','margin_left' => 10,'margin_right' => 10,'margin_top' => 10,'margin_bottom' => 10,'margin_header' => 0,'margin_footer' => 10,'orientation' => 'L']);
-$mpdf->WriteHTML($html);
-
-$mpdf->Output();
+<head>
+<script src="<?=BASE_URL?>assets/js/sweetalert2.all.min.js"></script>
+<link rel="stylesheet" href="<?=BASE_URL?>assets/css/sweetalert2.min.css">
+<head>
