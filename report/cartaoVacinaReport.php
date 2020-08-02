372c96548271719e61a473b06cd58238ed96de26
Thalles Rangel <ranglethr@gmail.com>
2020-08-01 23:20:14 -0300

    rela

---
+++ report/cartaoVacinaReport.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'vendor/autoload.php';
+// Create an instance of the class:
+$mpdf = new \Mpdf\Mpdf();
+
+// Write some HTML code:
+$mpdf->WriteHTML('Hello World');
+
+// Output a PDF file directly to the browser
+$mpdf->Output();
