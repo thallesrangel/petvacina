d2546584f00fe00c160b5959dcab2e8759ab58a0
Thalles Rangel <ranglethr@gmail.com>
2020-08-31 21:03:14 -0300

    aa

--- config.php
+++ config.php
@@ -6,13 +6,13 @@ $config = array();
 
 if(ENVIRONMENT == 'dev') {
     define("BASE_URL", "http://localhost:8080/petgestor/");
-    $config['dbname'] = 'petvacina';
+    $config['dbname'] = 'dbpetgestor';
     $config['host'] = 'localhost';
     $config['dbuser'] = 'root';
     $config['dbpass'] = '';
 } else {
     define("BASE_URL", "http://191.252.111.150/petgestor/");
-    $config['dbname'] = 'petvacina';
+    $config['dbname'] = 'dbpetgestor';
     $config['host'] = 'localhost';
     $config['dbuser'] = 'thalles';
     $config['dbpass'] = 'maax0710';
