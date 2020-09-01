1022f486620760ca5be8510a2d42df79592082e7
Thalles Rangel <ranglethr@gmail.com>
2020-09-01 12:59:40 -0300

    alt

--- views/404.php
+++ views/404.php
@@ -1,1 +1,21 @@
-<h1>Página não encontrada</h1>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" type="text/css" href="<?=BASE_URL?>assets/css/bootstrap.min.css"/>
+	<link type="text/css" rel="stylesheet" href="<?=BASE_URL?>assets/css/404.css" />
+	<link rel='icon' href='<?= BASE_URL?>assets/icon/icon.png' type='image/x-icon' sizes="16x16" />
+	<title>Pet Gestor</title>
+</head>
+<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>404</h1>
+			</div>
+			<h2>Oops! Página não encontrada</h2>
+			<p>Esta página não existe ou está temporariamente indisponível. <a href="<?=BASE_URL?>">Voltar para início</a></p>
+			<div class="notfound-social">
+				<a target="_blank" href="#"><img class="img-fluid pt-2" src="<?=BASE_URL?>assets/img/icon/facebook.svg"></a>
+				<a target="_blank" href="#"><img class="img-fluid pt-2" src="<?=BASE_URL?>assets/img/icon/instagram.svg"></a>
+			</div>
+		</div>
+	</div>
