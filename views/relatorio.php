6339e63d4e58dca3fe2b33b99dcd00e02908d17e
thallesrangel <thalles.lopes@uvv.br>
2020-12-09 15:26:10 -0300

    alterações

--- views/relatorio.php
+++ views/relatorio.php
@@ -1,10 +1,44 @@
 <div class="container bg-white rounded h-75">
     <h5 class="pl-3 pt-3 mb-0 h5-title-list">Relatórios</h5>
 
-    <ul>
-        <li> <a href="<?=BASE_URL?>vermifugacaoreport">Vermifugação</a> </li>
-        <li> <a href="<?=BASE_URL?>animalreport">Animal</a> </li>
-        <li> <a href="<?=BASE_URL?>cioreport">Anti-cio</a> </li>
-        
-    <ul>
+    <br>
+    <div class="container">
+        <div class="row text-center">
+
+            <div class="col-md-3 col-sm-12">
+                <div class="card-report card">
+                    <a href="<?=BASE_URL?>vermifugacaoreport">
+                        <div class="card-body">Vermifugação</div>
+                    </a>
+                </div>
+                </a>
+            </div>
+
+            <div class="col-md-3 col-sm-12">
+                <div class="card-report card">
+                    <a href="<?=BASE_URL?>animalreport">
+                        <div class="card-body">Animal</div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-12">
+                <div class="card-report card">
+                    <a href="<?=BASE_URL?>cioreport">
+                        <div class="card-body">Anti-cio</div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-12">
+                <div class="card-report card">
+                    <a href="<?=BASE_URL?>higienereport">
+                        <div class="card-body">Higiene</div>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
 </div>
