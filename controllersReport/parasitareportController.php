215a12e8aa213228b5ec0fc601153ce52bae86cd
thallesrangel <thalles.lopes@uvv.br>
2020-12-10 20:38:55 -0300

    relatório parasitas

---
+++ controllersReport/parasitareportController.php
@@ -0,0 +1,36 @@
+<?php
+
+class parasitareportController extends Controller
+{   
+    public function __construct()
+    {
+        if(empty($_SESSION['id_usuario'])){
+            header("Location: ".BASE_URL."login");
+        }
+    }
+    
+    public function render()
+    {   
+        if(empty($_POST)){
+            # Quando não há POST para gerar o relatório
+            $_SESSION['msg'] = 'report_sem_post';
+            header("Location: ".BASE_URL."parasitareport");
+        }
+        new ReportParasita();
+    }
+    
+    public function index()
+    {
+        $breadcrumb = [
+			'Início' => '',
+			'Relatório' => 'false',
+			'Parasita' => 'false'
+        ];
+
+        $proprietarios = new Proprietario();
+        $dados['proprietarios'] = $proprietarios->getAllResumido();
+
+        $this->setBreadCrumb($breadcrumb);
+        $this->loadTemplate('reportFormParasita', $dados);
+    }
+}
--- views/relatorio.php
+++ views/relatorio.php
@@ -42,13 +42,21 @@
 
         <div class="row text-center mt-4">
             <div class="col-md-3 col-sm-12">
-                    <div class="card-report card">
-                        <a href="<?=BASE_URL?>fornecedorreport">
-                            <div class="card-body">Fornecedor</div>
-                        </a>
-                    </div>
+                <div class="card-report card">
+                    <a href="<?=BASE_URL?>fornecedorreport">
+                        <div class="card-body">Fornecedor</div>
+                    </a>
                 </div>
             </div>
-    </div>
+            
+            <div class="col-md-3 col-sm-12">
+                <div class="card-report card">
+                    <a href="<?=BASE_URL?>parasitareport">
+                        <div class="card-body">Pulgas e Carrapatos</div>
+                    </a>
+                </div>
+            </div>
+        </div>
 
+    </div>
 </div>
