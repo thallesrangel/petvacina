22dd7f841bb2dcaa229475f6c4ee52f582d17353
thallesrangel <thalles.lopes@uvv.br>
2020-12-18 19:02:20 -0300

    aa

--- config.php
+++ config.php
@@ -16,7 +16,7 @@ if (ENVIRONMENT == 'dev') {
     $config['dbpass'] = '';
 } else {
     
-    define("BASE_URL", "http://191.252.202.186/app/");
+    define("BASE_URL", "http://191.252.202.186/petvac/");
     
     $config['dbname'] = 'petvac';
     $config['host'] = 'localhost';
