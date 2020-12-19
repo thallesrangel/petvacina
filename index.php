4d81907f81cd5cc7e82971be0b77414fc8dd16df
thallesrangel <thalles.lopes@uvv.br>
2020-12-18 22:49:49 -0300

    a

--- index.php
+++ index.php
@@ -1,31 +1,109 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="PetVacina">
+    <meta name="author" content="Thalles Rangel Lopes">
+    <title>Petvac | Software para acompanhamento de animais domésticos</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./public/css/style.css"/>
+    <link rel='icon' href="./public/img/favicon.png" type='image/x-icon' sizes="16x16" />
+</head>
+    <body>
+        
+        <nav class="navbar navbar-expand-lg bg-white shadow p-3 mb-2 bg-white rounded">
+            <a class="navbar-brand logo" href="#"><img class="img-fluid" src="./public/img/logo.png"></a>
 
-session_start();
+            <header class="ml-auto">
+                <!-- Responsive -->
+                <div class="toggleMenu" onclick="menuToggle()"></div>
+                
+                <ul class="navbar-nav navigation">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Adoção</a>
+                    </li>
 
-require "config.php";
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Saiba Mais</a>
+                    </li>
 
-spl_autoload_register(function($class){
-
-    if (file_exists('controllers/'.$class.'.php')) {
-        require 'controllers/'.$class.'.php';
-    } 
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-sm btn-primary" href="app/login">Acessar</a>
+                    </li>
+        
+                </ul>
+            </header>
     
-    else if (file_exists('controllersReport/'.$class.'.php')) {
-        require 'controllersReport/'.$class.'.php';
-    } 
+        </nav>
+        
+        <div class="container div-banner w-75">
+            <div class="row align-items-center">
+                <div class="col-md-5">
+                    <h1>A principal ferramenta de gestão de animais domésticos</h1>
+                    <a class="btn btn-sm btn-secondary" href="app/login">Registre-se Grátis</a>
+                </div>
 
-    else if (file_exists('report/'.$class.'.php')) {
-        require 'report/'.$class.'.php';
-    } 
-    
-    else if (file_exists('models/'.$class.'.php')){
-        require 'models/'.$class.'.php';
-    }
-    
-    else if (file_exists('core/'.$class.'.php')){
-        require 'core/'.$class.'.php';
-    }   
-});
+                <div class="col-md-7 d-flex justify-content-center">
+                    <img class="img-fluid" src="./public/img/bg.png">
+                </div>
+            </div>
+        </div>
+        
+        <div class="container text-center">
+            <div class="row d-flex justify-content-center">
+                <div class="col-sm-6 col-md-8">
+                    <h2>Registrar e acompanhar com rapidez.</h2>
+                    <p>O PetVac é pensando para que cada animal registrado seja devidamente acompanhando de forma ágil, eficaz e eficiente.</p>
+                </div>
+            </div>
+        </div>
 
-$core = new Core();
-$core->start();
+        <div class="container-fluid div-top">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-sm-12 col-md-7">
+                    <img class="img-fluid" src="./public/img/1.png"/>
+                </div>
+
+                <div class="col-sm-12 col-md-4">
+                    <h3>Animais</h3>
+                    <p>Registre e acompanhe vacinas, vermifugos, parasitas, anti-cio, higiene, métricas, peso e mais.</p>
+ 
+                    
+                    <h3 class="mt-5">Relatórios</h3>
+                    <p>Trás a facilidade de acompanhar todo o histórico do animal.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid div-top">
+            <div class="row align-items-center justify-content-center">
+
+                <div class="col-sm-12 col-md-4">
+                    <h3>Proprietários</h3>
+                    <p>Animais registrados por proprietário, permitindo maior acompnhamento do animal.</p>
+                    
+                    <h3 class="mt-5">Vacinas</h3>
+                    <p>Vacine e tenha todo o histórico de vacinação de seus animais. Permite agendar vacinações sendo notificado na data definida.</p>
+                    
+                </div>
+
+                <div class="col-sm-12 col-md-7">
+                    <img class="img-fluid" src="./public/img/2.png"/>
+                </div>
+            </div>
+        </div>
+        
+        
+        <script type="text/javascript">
+        /* Responsive - Define como ativo a classe toggleMenu */
+            function menuToggle(){
+                const toggleMenu = document.querySelector('.toggleMenu');
+                const navigation = document.querySelector('.navigation');
+                toggleMenu.classList.toggle('active')
+                navigation.classList.toggle('active')
+            }
+        </script>
+    </body>
+</html>
