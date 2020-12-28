9e3b2c3b0ea3cab5e97597cdc8d1fa4f4e983401
thallesrangel <thalles.lopes@uvv.br>
2020-12-27 22:33:29 -0300

    a

--- views/home.php
+++ views/home.php
@@ -1,4 +1,3 @@
-
 <?php
 
     $qtd_animais_dados = [];
@@ -16,37 +15,38 @@
     foreach($qtd_vermifugacao as $item) {
         $qtd_vermifugacao_dados = $item;
     }
-?>   
+?>
+
 <div class="container">
 
     <div class="row justify-content-center">
 
         <div class="col-sm-12 col-md-3 mt-2">
             <div class="card border-left-primary shadow py-2">
-                <div class="card-body text-center">
-                    <img src="<?=BASE_URL?>assets/img/pata.png" width="75" height="75" class="img-responsive">
-                    <h3><?= $qtd_animais_dados['qtd'] ?></h3>
-                    <p class="m-0">Animais Registrados</p>
+                <div class="card-body">
+                    <img src="<?=BASE_URL?>assets/img/pata.png" width="55" height="55" class="img-responsive">
+                    <span class="h3 ml-5"><?= $qtd_animais_dados['qtd'] ?></span>
+                    <p class="m-1">Animais registrados</p>
                 </div>
             </div>
         </div>
 
         <div class="col-sm-12 col-md-3 mt-2">
             <div class="card border-left-primary shadow py-2">
-                <div class="card-body text-center">
-                    <img src="<?=BASE_URL?>assets/img/seringa.png" width="75" height="75" class="img-responsive">
-                    <h3><?= $qtd_vacinas_dados['qtd'] ?></h3>
-                    <p class="m-0">Vacinas Aplicadas</p>
+                <div class="card-body">
+                    <img src="<?=BASE_URL?>assets/img/seringa.png" width="55" height="55" class="img-responsive">
+                    <span class="h3 ml-5"><?= $qtd_vacinas_dados['qtd'] ?></span>
+                    <p class="m-1">Vacinas aplicadas</p>
                 </div>
             </div>
         </div>
 
         <div class="col-sm-12 col-md-3 mt-2">
             <div class="card border-left-primary shadow py-2">
-                <div class="card-body text-center">
-                    <img src="<?=BASE_URL?>assets/img/escudo.png" width="75" height="75" class="img-responsive">
-                    <h3><?= $qtd_vermifugacao_dados['qtd'] ?></h3>
-                    <p class="m-0">Vermífugo Aplicados</p>
+                <div class="card-body">
+                    <img src="<?=BASE_URL?>assets/img/escudo.png" width="55" height="55" class="img-responsive">
+                    <span class="h3 ml-5"><?= $qtd_vermifugacao_dados['qtd'] ?></span>
+                    <p class="m-1">Vermífugo aplicados</p>
                 </div>
             </div>
         </div>
