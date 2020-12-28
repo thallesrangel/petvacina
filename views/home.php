db6bfaf14aa8c28f9d5648792f88fb28ad690bf0
thallesrangel <thalles.lopes@uvv.br>
2020-12-28 18:24:40 -0300

    a

--- views/home.php
+++ views/home.php
@@ -23,31 +23,38 @@
 
         <div class="col-sm-12 col-md-3 mt-2">
             <div class="card border-left-primary shadow py-2">
-                <div class="card-body text-center">
-                    <img src="<?=BASE_URL?>assets/img/pata.png" width="60" height="60" class="img-responsive">
-                    <span class="h3 ml-5"><?= $qtd_animais_dados['qtd'] ?></span>
-                    <p class="m-2">Animais registrados</p>
-                </div>
+                <a href="<?=BASE_URL?>animal">
+                    <div class="card-body text-center">
+                        <img src="<?=BASE_URL?>assets/img/pata.png" width="60" height="60" class="img-responsive">
+                        <span class="h3 ml-5"><?= $qtd_animais_dados['qtd'] ?></span>
+                        <p class="m-2">Animais registrados</p>
+                    </div>
+                </a>
             </div>
         </div>
 
         <div class="col-sm-12 col-md-3 mt-2">
             <div class="card border-left-primary shadow py-2">
-                <div class="card-body text-center">
-                    <img src="<?=BASE_URL?>assets/img/seringa.png" width="60" height="60" class="img-responsive">
-                    <span class="h3 ml-5"><?= $qtd_vacinas_dados['qtd'] ?></span>
-                    <p class="m-2">Vacinas aplicadas</p>
-                </div>
+                <a href="<?=BASE_URL?>vacina">
+                    <div class="card-body text-center">
+                        <img src="<?=BASE_URL?>assets/img/seringa.png" width="60" height="60" class="img-responsive">
+                        <span class="h3 ml-5"><?= $qtd_vacinas_dados['qtd'] ?></span>
+                        <p class="m-2">Vacinas aplicadas</p>
+                    </div>
+                </a>
             </div>
+            
         </div>
 
         <div class="col-sm-12 col-md-3 mt-2">
             <div class="card border-left-primary shadow py-2">
-                <div class="card-body text-center">
-                    <img src="<?=BASE_URL?>assets/img/escudo.png" width="60" height="60" class="img-responsive">
-                    <span class="h3 ml-5"><?= $qtd_vermifugacao_dados['qtd'] ?></span>
-                    <p class="m-2">Vermífugo aplicados</p>
-                </div>
+                <a href="<?=BASE_URL?>vermifugacao">
+                    <div class="card-body text-center">
+                        <img src="<?=BASE_URL?>assets/img/escudo.png" width="60" height="60" class="img-responsive">
+                        <span class="h3 ml-5"><?= $qtd_vermifugacao_dados['qtd'] ?></span>
+                        <p class="m-2">Vermífugo aplicados</p>
+                    </div>
+                </a>
             </div>
         </div>
     
